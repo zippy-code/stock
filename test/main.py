af456289ce911e4fc05cc32fb0776444587be9e5
seonjoo.kim <seonjoo1128@naver.com>
2021-11-19 11:19:02 +0900

    네이버 뉴스를  단어, 정렬 방법, 검색 할 뉴스 개수를  입력 받아서 크롤링

--- test/main.py
+++ test/main.py
@@ -6,15 +6,19 @@ import pandas
 import re
 
 
-resultList = []
-
+query = input('검색 키워드를 입력하세요 : ') 
+news_num = int(input('필요한 뉴스기사의 수를 입력하세요(숫자만 입력) : '))
+sort = int(input('관련도순(0), 최신순(1), 오래된순(2)(숫자만 입력) : '))
 url = "https://search.naver.com/search.naver?"
+#url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + s_date + "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
 
 params = {
     "where": 'news',
 
     # 네이버 기사 검색 값
-    "query": '경제',
+    "query": query,
+    
+    "sort": sort,
 
     # 페이지네이션 값
     "start": 0,
@@ -31,52 +35,38 @@ params = {
 raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, params=params)
 html = BeautifulSoup(raw.text, "html.parser")
 
-# 검색결과 html body
-articles = html.select("ul.type01 > li")
+print('크롤링 중...')
+idx = 0
+news_dict = {}
+cur_page = 1
 
-# 전체 기사 수
-totalCount = 0
-if html.select("div.section_head > div.title_desc > span"):
-    totalCount = html.select("div.section_head > div.title_desc > span")[0].text.split(' / ')[1][:-1]
+while idx < news_num:
+    table = html.find('ul', {'class' : 'list_news'})
+    li_list = table.find_all('li', {'id': re.compile('sp_nws.*')})
+    area_list = [li.find('div', {'class' : 'news_area'}) for li in li_list]
+    a_list = [area.find('a', {'class' : 'news_tit'}) for area in area_list]
+    
+    idx_in = 0
+    for n in a_list[:min(len(a_list), news_num-idx)]:
+        news_dict[idx] = {'title' : n.get('title'),
+                          'desc' : area_list[idx_in].select_one('div.news_wrap.api_ani_send > div.news_area > div.news_dsc > div.dsc_wrap > a').text,
+                          'url' : n.get('href'), }
+        idx_in += 1
+        idx += 1
 
-for i in range(0, math.floor(int(totalCount)/10)+1):
-    if i == 0:
-        params['start'] = i
-    else:
-        params['start'] = i * 10 + 1
+    cur_page += 1
 
-    raw = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'}, params=params)
-    html = BeautifulSoup(raw.text, "html.parser")
-    articles = html.select("ul.type01 > li")
+    pages = html.find('div', {'class' : 'sc_page_inner'})
+    next_page_url = [p for p in pages.find_all('a') if p.text == str(cur_page)][0].get('href')
+    
+    req = requests.get('https://search.naver.com/search.naver' + next_page_url)
+    html = BeautifulSoup(req.text, 'html.parser')
 
-    for ar in articles:
-        # 제목 값
-        title = ar.select_one("a._sp_each_title").text
 
-        # 검색된 기사의 url을 가져와서 다시 html을 get
-        articleUrl = ar.find("a")["href"]
-        innerRaw = requests.get(articleUrl, headers={'User-Agent': 'Mozilla/5.0'})
+print('크롤링 완료')
 
-        # 가져온 기사 html중 '기사', '@' string을 모두 가져온다
-        innerHtml = BeautifulSoup(innerRaw.text, "html.parser")
-        reporter = innerArticles = innerHtml(text=re.compile("기자"))
-        reporterEmail = innerArticles = innerHtml(text=re.compile("@"))
+print('데이터프레임 변환')
+news_df = pandas.DataFrame(news_dict).T
 
-        # 언론사 값
-        source = ar.select_one("span._sp_each_source").text
-
-        # 등록일 값
-        date = ar.select_one("dd.txt_inline").text.split("  ")[1]
-
-        res = {"title": title, "company": source,
-               "url": articleUrl, "date": date, "reporter": reporter, "reporterEmail": reporterEmail}
-        resultList.append(res)
-
-# 검색된 기사 갯수
-resultList.append({"totalCount": totalCount})
-
-df = pandas.DataFrame(resultList)
-
-df.to_csv('blockChain_articles.csv')
-df.to_excel('blockChain_articles.xlsx')
-
+news_df.to_csv('blockChain_articles.csv')
+news_df.to_excel('blockChain_articles.xlsx')
